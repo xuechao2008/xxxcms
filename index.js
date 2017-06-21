339538b2735778c0424fc9c8ad6739ade2bb79ca
xuechao <xuechao@netnic.com.cn>
2017-06-21 15:19:01 +0800

    实例 热更新 webpack-dev-server 使用

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
  * Created by 6503 on 2017/6/21.
  */
 document.addEventListener('DOMContentLoaded',function () {
-    console.log('hi')
+    console.log('34343434');
+
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 /**
  * Created by 6503 on 2017/6/21.
  */
+var webpack =require('webpack');
+
 module.exports={
     entry:'./index.js',
     output:{
-        filename:'bundle.js'//打包输出的文件是bundle.js
-    }
+        filename:'bundle.js'
+    },
+    devServer: {
+        historyApiFallback:true,
+        hot:true,
+        inline:true,
+        open: true
+    },
+    plugins:[
+
+    ]
 }
