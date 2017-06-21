e08fa87eae0a89aea67250871267b8a696238232
xuechao <xuechao@netnic.com.cn>
2017-06-21 10:40:26 +0800

    测试实例1

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by 6503 on 2017/6/21.
+ */
+document.addEventListener('DOMContentLoaded',function () {
+    console.log('hi')
+})
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 6503 on 2017/6/21.
+ */
+module.exports={
+    entry:'./index.js',
+    output:{
+        filename:'bundle.js'//打包输出的文件是bundle.js
+    }
+}
